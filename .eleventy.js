5eaedd61de18cb74c9a8065f65eabade9b0bc13b
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2021-11-25 11:30:03 +0100

    project, design and content updates

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addWatchTarget("src/css/*.css");
 
   // export core cupcake.css
-  eleventyConfig.addPassthroughCopy({"src/css/cupcake.css" : "cupcake.css"});
+  eleventyConfig.addPassthroughCopy({"src/css/cupcake.css" : "./cupcake.css"});
 
   // export for flavors for later
   // eleventyConfig.addPassthroughCopy({"./cupcake-brownie.css" : "cupcake-brownie.css"});
