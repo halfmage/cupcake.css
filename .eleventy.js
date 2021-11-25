7ae03af4693b9f6ca7673a02f1fb51f796e330df
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2021-11-25 11:10:04 +0100

    project cleanup

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,22 @@
 module.exports = function(eleventyConfig) {
 
   // watch target for quick previews
-  eleventyConfig.addWatchTarget("./*.css");
+  eleventyConfig.addWatchTarget("src/css/*.css");
 
   // export core cupcake.css
-  eleventyConfig.addPassthroughCopy({"./cupcake.css" : "cupcake.css"});
+  eleventyConfig.addPassthroughCopy({"src/css/cupcake.css" : "cupcake.css"});
 
-  // export for flavors
-  eleventyConfig.addPassthroughCopy({"./cupcake-brownie.css" : "cupcake-brownie.css"});
+  // export for flavors for later
+  // eleventyConfig.addPassthroughCopy({"./cupcake-brownie.css" : "cupcake-brownie.css"});
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      layouts: "layouts",
+      includes: "includes",
+    },
+    htmlTemplateEngine: "njk"
+  }
   
 };
